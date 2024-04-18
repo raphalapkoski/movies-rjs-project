c88f413dab2c9aaf6eb1c6bc8bd2c44524b0dc55
Raphael Lapkoski <raphael.projetos@outlook.com.br>
2024-04-18 00:50:14 -0300

    Adicionando página para aviso de "página não encontrada"

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,10 @@
+import './not-found.css'
+
+export default function NotFound(){
+    return (
+        <div className="not-found">
+            <h1>404</h1>
+            <strong>Página não encontrada!</strong>
+        </div>
+    );
+}
