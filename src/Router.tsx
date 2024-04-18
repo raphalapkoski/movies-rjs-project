1a90b8947bdc9b290dd4060bf21960eb3ae1e905
Raphael Lapkoski <raphael.projetos@outlook.com.br>
2024-04-18 00:50:30 -0300

    Adicionando rotas e chamada para api.

---
+++ src/Router.tsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+
+export default function Router() {
+    return(
+        <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='*' element={<NotFound/>} />
+        </Routes>
+    );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
+
 import './index.css'
+
+import Router from './Router'
+import Header from './components/Header'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Header/>
+      <Router/>
+    </BrowserRouter>
   </React.StrictMode>,
 )
