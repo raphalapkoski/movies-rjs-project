2b3c2da331814aa38ffc129b8718e0530cfbf9f0
Raphael Lapkoski <raphael.projetos@outlook.com.br>
2024-04-18 23:18:44 -0300

    Adiconando funcionalidade de visualizar o trailer do filme

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -8,36 +8,56 @@ import api from '../../services/api';
 export default function Details() {
 
     let path = 'https://image.tmdb.org/t/p/original';
+    let youtube = 'https://www.youtube.com/results?search_query';
     const { id } = useParams();
-    const [details, setDetails] = useState({});
+    const [movie, setMovie] = useState({});
 
     useEffect(() => {
         loadDetails();
     });
 
     async function loadDetails() {
-        await api.get(`/${id}`, {
+        await api.get(`/movie/${id}`, {
             params: {
-                api_key: "c1205111bb4dc67b34e32c2ac3dddd67",
+                api_key: "f18f7cd7a1d32a95a4b9332ec74bc8ab",
                 language: "pt-BR",
             }
         }).then((response) => {
-            setDetails(response.data);
+            setMovie(response.data);
         }).catch(() => {
             console.log("erro")
         });
     }
 
+    async function addFavorite() {
+        const list = localStorage.getItem('@moviesrjs');
+        
+        let moviesSaved = list ? JSON.parse(list) : [];
+
+        const movieExists = moviesSaved.some((movie) => movie.id == movie.id);
+
+        if(movieExists){
+            alert('Este filme já está em sua lista de favoritos');
+            return;
+        }
+
+        moviesSaved.push(movie);
+        localStorage.setItem('@moviesrjs', JSON.stringify(moviesSaved));
+        alert("Filme salvo em sua lista de favoritos");
+    }
+
     return (
-        <div className='container' style={{ backgroundImage: `url(${path}/${details.backdrop_path})` }}>
+        <div className='container' style={{ backgroundImage: `url(${path}/${movie.backdrop_path})` }}>
             <div className='details-text-align'>
-                <h1>{details.title}</h1>
-                <strong>{details.overview}</strong>
-                <strong className='vote'>Avaliação {details.vote_average}/10</strong>
+                <h1>{movie.title}</h1>
+                <strong>{movie.overview}</strong>
+                <strong className='vote'>Avaliação {movie.vote_average} / 10</strong>
             </div>
             <div className='buttons-align'>
-                <button>Assistir trailer</button>
-                <button>Adicionar ao favoritos</button>
+                <button>
+                    <a target='blank' href={`${youtube}=${movie.title} Trailer`}>Assistir trailer</a>
+                </button>
+                <button onClick={addFavorite}>Adicionar ao favoritos</button>
             </div>
         </div>
     )
